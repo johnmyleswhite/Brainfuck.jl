8751c2b218f67612d636dc642267e3af3ee02ce3
John Myles White <jmw@johnmyleswhite.com>
2014-02-26 23:06:35 -0800

    Add 99 bottles test

--- test/programs.jl
+++ test/programs.jl
@@ -7,4 +7,8 @@ module TestPrograms
     io = IOBuffer()
     Brainfuck.interpret(ops, io_out = io)
     @test takebuf_string(io) == "Hello world!\n"
+
+    text = readall(Pkg.dir("Brainfuck", "test", "programs", "99bottles.bfk"))
+    io = IOBuffer()
+    Brainfuck.brainfuck(text, io_out = io)
 end
